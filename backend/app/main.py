4470031e070f28835b16f3a986fa911ccf594521
aravindd222 <aravindrajapvt@gmail.com>
2026-01-12 10:16:10 +0530

    fix: fix done

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routers import auth, post, social
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Social Media Publisher Demo")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(post.router)
