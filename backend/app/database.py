ef82434b8d1164b0f55f222313f400569b9ac097
aravindd222 <aravindrajapvt@gmail.com>
2026-02-06 11:37:26 +0530

    FIX:

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,8 +4,6 @@ from app.config import settings
 
 
 
-DATABASE_URL = "postgresql://postgres:postgre123@localhost:5432/social_publisher_db"
-
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine) 
 Base = declarative_base()
