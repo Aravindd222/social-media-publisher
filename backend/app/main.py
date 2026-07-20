a4f453e7b3b24fdf058a7fd378c1e9a5efae7614
aravindd222 <aravindrajapvt@gmail.com>
2026-07-20 18:47:43 +0530

    FIX: CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,10 @@ app = FastAPI(title="Social Media Publisher Demo")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=[
+        "http://localhost:5173",
+        "https://social-frontend-q0te.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
